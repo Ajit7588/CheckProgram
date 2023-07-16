1f59c970592a5e4a4bf5b8935f359b03b4e5913b
AjitKumarTung <ajitkumartung@gmail.com>
2023-07-16 20:43:07 +0530

    new work

---
+++ src/test/java/demowork/DemoWork.java
@@ -0,0 +1,26 @@
+package demowork;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoWork 
+{
+	 @Test(priority = 3)
+	  public void flipkart() 
+	  {
+		  
+		  WebDriverManager.chromedriver().setup();
+		  ChromeOptions option=new ChromeOptions();
+		  option.addArguments("--remote-allow-origins=*");
+		  WebDriver driver=new ChromeDriver(option);
+		  
+		  driver.get("https://www.flipkart.com");
+		  
+
+		  
+	  }
+}
