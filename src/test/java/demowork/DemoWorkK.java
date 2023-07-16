333e7600c8173fd1980321f35fa4123e66218179
AjitKumarTung <ajitkumartung@gmail.com>
2023-07-16 21:36:13 +0530

    Add file

--- src/test/java/demowork/DemoWorkK.java
+++ src/test/java/demowork/DemoWorkK.java
@@ -1,9 +1,25 @@
 package demowork;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DemoWorkK {
   @Test
-  public void f() {
+  public void f() 
+  {
+	  WebDriverManager.chromedriver().setup();
+	  ChromeOptions option=new ChromeOptions();
+	  option.addArguments("--remote-allow-origins=*");
+	  WebDriver driver=new ChromeDriver(option);
+	  
+	  driver.get("https://www.google.com");
+	  
+	  
+	  
+	  
   }
 }
